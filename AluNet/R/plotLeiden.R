f9c3f461ac1787a83d46322e95b8c40752f4c572
babessell1 <babessell@gmail.com>
2023-12-15 03:03:42 -0500

    update report and plotting

--- AluNet/R/plotLeiden.R
+++ AluNet/R/plotLeiden.R
@@ -3,7 +3,7 @@
 #' @param result The result of the runLeiden function.
 #' @return A plot of the graph with nodes colored based on community.
 #' @export
-plotLeiden <- function(result) {
+plotLeiden <- function(result, Alus = FALSE) {
     gdf <- graph.data.frame(result$graph)
 
     # get node indices from the graph
@@ -26,21 +26,36 @@ plotLeiden <- function(result) {
     colors <- community_colors[as.factor(V(gdf)$membership)]
 
     # Plot the graph
-    plot(
-    gdf,
-    layout = layout_with_fr(gdf),
-    vertex.color = colors,  # Color nodes based on community
-    vertex.frame.color = colors,
-    #edge.color = color_palette[cut(E(gdf)$weight, breaks = 100)],  # Map edge color from cold to hot
-    vertex.label = V(gdf)$name,
-    edge.arrow.size = 0,  # Set arrow size to 0 to remove arrows
-    main = "Our Rcpp-based Clustering"
-    )
-
-    # Add a legend for community colors
-    legend("topright", legend = unique(V(gdf)$membership),
+    if (!Alus) {
+        plot(
+            gdf,
+            layout = layout_with_fr(gdf),
+            vertex.color = colors,  # Color nodes based on community
+            vertex.frame.color = colors,
+            #edge.color = color_palette[cut(E(gdf)$weight, breaks = 100)],  # Map edge color from cold to hot
+            vertex.label = V(gdf)$name,
+            edge.arrow.size = 0,  # Set arrow size to 0 to remove arrows
+            main = "Our Rcpp-based Clustering"
+        )
+        # Add a legend for community colors
+        legend("topright", legend = unique(V(gdf)$membership),
         col = community_colors, pch = 19,
         title = "Communities", cex = 0.8)
+        
+    } else {
+        plot(
+            gdf,
+            layout = layout_with_fr(gdf),
+            vertex.color = colors,  # Color nodes based on community
+            vertex.frame.color = colors,
+            # make vertices smaller
+            vertex.size = 0.25,
+            #edge.color = color_palette[cut(E(gdf)$weight, breaks = 100)],  # Map edge color from cold to hot
+            vertex.label = NA,
+            edge.arrow.size = 0,  # Set arrow size to 0 to remove arrows
+            main = "Our Rcpp-based Clustering"
+        )
+    }
 
 }
 
